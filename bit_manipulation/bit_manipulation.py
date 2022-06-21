290b1e3ceba7fe39a3480e2f3b9ec0e9752e362a
Mustafa <78226423+0mustafa@users.noreply.github.com>
2022-06-21 14:52:02 +0300

    Create bit_manipulation.py

---
+++ bit_manipulation/bit_manipulation.py
@@ -0,0 +1,18 @@
+vars = [4,4,3,3,1,6,6,8,8]
+zero = 0
+
+for i in vars:
+    ###### xor ######
+    #   | 0  1
+    # 0 | 0  1
+    # 1 | 1  0
+    #################
+    
+    # ^ is xor symbol in python
+
+    #zero xor i 
+    #000 xor 100 = 100
+    #100 xor 100 = 000
+    zero = zero ^ i
+
+print(zero)
